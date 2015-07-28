f0f0821f1c6dd8782593ee767a89efcdecf97269
Tom Rochette <trochette@exolnet.com>
2015-07-28 11:41:43 -0400

    Add set -u to setup.sh Use ${i} instead of $i to expand the version number.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+set -u
+
 BASE=`pwd`
 for i in 7 8 9
 do
-	TARGET=$HOME/Library/Preferences/WebIde$i0
+	TARGET=$HOME/Library/Preferences/WebIde${i}0
 	mkdir -p $TARGET/codestyles $TARGET/templates
 	ln -s $BASE/codestyles/eXolnet.xml $TARGET/codestyles/eXolnet.xml
 	ln -s $BASE/templates/eXolnet.xml $TARGET/templates/eXolnet.xml
