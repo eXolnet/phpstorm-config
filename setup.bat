92c13d9287eb388c6c61dd8782c83c33400c74ea
Tom Rochette <trochette@exolnet.com>
2016-08-30 17:09:58 -0400

    Add support for PHPStorm 2016.1 and 2016.2.

--- setup.bat
+++ setup.bat
@@ -6,3 +6,9 @@ for %%i in (7 8 9 10 11) do (
 	mklink !TARGET!\codestyles\eXolnet.xml %BASE%\codestyles\eXolnet.xml
 	mklink !TARGET!\templates\eXolnet.xml %BASE%\templates\eXolnet.xml
 )
+
+for %%s in (2016.1 2016.2) do (
+	set TARGET=%USERPROFILE%\.PhpStorm%%s\config
+	mklink !TARGET!\codestyles\eXolnet.xml %BASE%\codestyles\eXolnet.xml
+	mklink !TARGET!\templates\eXolnet.xml %BASE%\templates\eXolnet.xml
+)
--- setup.sh
+++ setup.sh
@@ -9,3 +9,11 @@ do
 	ln -s $BASE/codestyles/eXolnet.xml $TARGET/codestyles/eXolnet.xml
 	ln -s $BASE/templates/eXolnet.xml $TARGET/templates/eXolnet.xml
 done
+
+for i in 2016.1 2016.2
+do
+	TARGET=$HOME/Library/Preferences/PhpStorm${i}
+	mkdir -p $TARGET/codestyles $TARGET/templates
+	ln -s $BASE/codestyles/eXolnet.xml $TARGET/codestyles/eXolnet.xml
+	ln -s $BASE/templates/eXolnet.xml $TARGET/templates/eXolnet.xml
+done
